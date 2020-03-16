74df6abf13b44b69fd9b1a7568ecb413939350dc
Vivian Wan <vivianpswan@gmail.com>
2020-03-16 15:25:25 +1100

    included articles page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   delete '/login' => 'session#destroy'
 
   resources :students
+  resources :articles
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_235955) do
+ActiveRecord::Schema.define(version: 2020_03_16_041401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "programs", force: :cascade do |t|
     t.text "stage"
