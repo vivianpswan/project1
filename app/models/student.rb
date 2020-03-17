d3d42af6f3fcd5fa51ca91d9c29571983395c3b1
Vivian Wan <vivianpswan@gmail.com>
2020-03-18 10:26:12 +1100

    association between students has many program through results are not working :<

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
     has_and_belongs_to_many :tutors
+    has_and_belongs_to_many :programs
     has_many :results
-    has_many :programs, :through => :results
 end
