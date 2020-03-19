b65296b73892f7bf650d8f07d97a2a6eb643c82e
Vivian Wan <vivianpswan@gmail.com>
2020-03-20 10:44:26 +1100

    added info to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,20 @@
 
 Tutor.destroy_all
 t1 = Tutor.create :name => 'Tom Hanks', :stage => 'Year 2', :subject => 'Maths', :email => 'tomhanks@gmail.com', :password => 'chicken', :admin => true 
-t2 = Tutor.create :name => 'Rita Hanks', :stage => 'Year 2', :subject => 'English', :email => 'ritahanks@gmail.com', :password => 'chicken'
+t2 = Tutor.create :name => 'Rita Hanks', :stage => 'Year 2', :subject => 'English', :email => 'ritahanks@gmail.com', :password => 'chicken', :admin => true
 t3 = Tutor.create :name => 'Carol Hanks', :stage => 'Year 1', :subject => 'Maths', :email => 'carolhanks@gmail.com', :password => 'chicken'
+t4 = Tutor.create :name => 'Daphane Hanks', :stage => 'Year 1', :subject => 'English', :email => 'carolhanks@gmail.com', :password => 'chicken'
+
 
 puts "#{ Tutor.count } tutors created"
 
 Student.destroy_all
-s1 = Student.create :name => 'Elizabeth Hanks', :image => 'https://i.pinimg.com/originals/e8/a3/65/e8a365a60c2b334b91354bcd8c98e484.jpg', :age => '5', :stage => 'Year 1'
-s2 = Student.create :name => 'Olivia Hanks', :image => 'https://i.pinimg.com/236x/92/e7/ed/92e7ed115a38faa54758a5f2d4ef15bb--beautiful-little-girls-pretty-girls.jpg', :age => '7', :stage => 'Year 2'
-s3 = Student.create :name => 'Jasper Hanks', :image => 'https://thumbs.dreamstime.com/t/reading-book-little-child-lying-down-floor-25845149.jpg', :age => '7', :stage => 'Year 2'
-s4 = Student.create :name => 'Anthony Hanks', :image => 'https://ak7.picdn.net/shutterstock/videos/4582967/thumb/1.jpg', :age => '6', :stage => 'Year 2'
-s5 = Student.create :name => 'Abby Hanks', :image => 'https://www.bestbrainscumming.com/images/best-brains-teaching.png', :age => '7', :stage => 'Year 2'
-s6 = Student.create :name => 'Isabella Hanks', :image => 'https://www.melbournechildpsychology.com.au/wp-content/uploads/2014/01/Girl-with-books-and-magnifier.jpg', :age => '5', :stage => 'Year 1'
+s1 = Student.create :name => 'Elizabeth Hanks', :image => 'https://image.shutterstock.com/image-photo/6-years-asian-girl-smile-260nw-448403773.jpg', :age => '5', :stage => 'Year 1'
+s2 = Student.create :name => 'Olivia Hanks', :image => 'https://image.shutterstock.com/image-photo/portrait-cute-7-years-old-260nw-1101299165.jpg', :age => '7', :stage => 'Year 2'
+s3 = Student.create :name => 'Jasper Hanks', :image => 'https://image.shutterstock.com/image-photo/smiling-boy-six-years-old-260nw-455866204.jpg', :age => '7', :stage => 'Year 3'
+s4 = Student.create :name => 'Anthony Hanks', :image => 'https://image.shutterstock.com/image-photo/cute-seven-year-old-filipino-260nw-311421431.jpg', :age => '6', :stage => 'Year 4'
+s5 = Student.create :name => 'Abby Hanks', :image => 'https://image.shutterstock.com/image-photo/portrait-cute-7-years-old-260nw-1101612335.jpg', :age => '7', :stage => 'Year 2'
+s6 = Student.create :name => 'Isabella Hanks', :image => 'https://image.shutterstock.com/image-photo/outdoor-portrait-cute-little-89-260nw-506899096.jpg', :age => '5', :stage => 'Year 1'
 
 puts "#{ Student.count } students created"
 
@@ -39,10 +41,11 @@ puts "Tutors and students"
 t1.students << s2 << s3
 t2.students << s4 << s5
 t3.students << s1 << s6
+t4.students << s1 << s6
 
 puts "Results and programs"
 r1.programs << p1 
-r2.programs << p2 
+r2.programs << p4 
 r3.programs << p1 << p2 << p3 << p4
 r4.programs << p1 << p2 << p3 << p4
 r5.programs << p1 << p2 << p3 << p4
@@ -51,4 +54,7 @@ r6.programs << p1 << p2 << p3 << p4
 puts "Students and results"
 s1.results << r1
 s2.results << r2
+s3.results << r3
+s4.results << r4
+
 
